32b9c59dedbf7ebe9a28acd286424e7f5a48c942
René Scheibe <rene.scheibe@gmail.com>
2011-07-30 23:35:53 +0200

    fixed building the plugin (.class files have been missing in the generated .jar) - PDE does not support Groovy classes out of the box - a special compiler adapater is required (configured in build.properties) - for more details see http://contraptionsforprogramming.blogspot.com/2010/08/groovy-pde-redux.html

---
+++ plugin/src/org/codenarc/eclipse/Dummy.java
@@ -0,0 +1,7 @@
+package org.codenarc.eclipse;
+
+/**
+ * Minimum one Java file is required to make the PDE builder compile Groovy files.
+ * @see http://contraptionsforprogramming.blogspot.com/2010/08/groovy-pde-redux.html
+ */
+class Dummy {}
