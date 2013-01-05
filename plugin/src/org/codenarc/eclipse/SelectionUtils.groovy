eebd43a30a6854e523aa36dc70eb0c1287bc6777
René Scheibe <rene.scheibe@gmail.com>
2013-01-05 23:45:43 +0100

    issue #3: Grails plugin sources are now excluded from code analysis
    - using the linked resource '.link_to_grails_plugins'
      (automatically created for a Grails project in Eclispe)

--- plugin/src/org/codenarc/eclipse/SelectionUtils.groovy
+++ plugin/src/org/codenarc/eclipse/SelectionUtils.groovy
@@ -7,6 +7,9 @@ import org.eclipse.jface.viewers.IStructuredSelection
 
 class SelectionUtils {
 
+    private static final GROOVY_FILE_EXTENSION = 'groovy'
+    private static final GRAILS_LINKED_RESOURCES_NAME = '.link_to_grails_plugins'
+
     static List<IFile> getGroovyFiles(IStructuredSelection selection) {
         def files = []
         addFileResources(selection.toList(), files)
@@ -16,13 +19,13 @@ class SelectionUtils {
 
     private static void addFileResources(resources, files) {
         for (IResource resource in resources) {
-            if (!resource.isAccessible()) {
+            if (!resource.isAccessible() || resource.name == GRAILS_LINKED_RESOURCES_NAME) {
                 continue
             }
 
             if (resource instanceof IFile) {
                 IFile file = resource
-                if (file.type == IResource.FILE && file.fileExtension == 'groovy') {
+                if (file.type == IResource.FILE && file.fileExtension == GROOVY_FILE_EXTENSION) {
                     files << file
                 }
             } else if (resource instanceof IContainer) {
