844fdc4afa1912adb22ab1be5d5c8d5cb8715d29
Csaba Sulyok <csaba.sulyok@gmail.com>
2013-02-17 19:44:03 +0100

    Add preference page and popup item to set custom XML ruleset file.

--- plugin/src/org/codenarc/eclipse/SelectionUtils.groovy
+++ plugin/src/org/codenarc/eclipse/SelectionUtils.groovy
@@ -8,6 +8,7 @@ import org.eclipse.jface.viewers.IStructuredSelection
 class SelectionUtils {
 
     private static final GROOVY_FILE_EXTENSION = 'groovy'
+    private static final XML_FILE_EXTENSION = 'xml'
     private static final GRAILS_LINKED_RESOURCES_NAME = '.link_to_grails_plugins'
 
     static List<IFile> getGroovyFiles(IStructuredSelection selection) {
@@ -17,7 +18,14 @@ class SelectionUtils {
         files
     }
 
-    private static void addFileResources(resources, files) {
+    static IFile getSingleGroovyFile(IStructuredSelection selection) {
+        def files = []
+        addFileResources(selection.toList(), files, XML_FILE_EXTENSION)
+        assert files.size() == 1
+        files[0]
+    }
+
+    private static void addFileResources(resources, files, expectedExtension = GROOVY_FILE_EXTENSION) {
         for (IResource resource in resources) {
             if (!resource.isAccessible() || resource.name == GRAILS_LINKED_RESOURCES_NAME) {
                 continue
@@ -25,7 +33,7 @@ class SelectionUtils {
 
             if (resource instanceof IFile) {
                 IFile file = resource
-                if (file.type == IResource.FILE && file.fileExtension == GROOVY_FILE_EXTENSION) {
+                if (file.type == IResource.FILE && file.fileExtension == expectedExtension) {
                     files << file
                 }
             } else if (resource instanceof IContainer) {
