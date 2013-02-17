5ed0b2a65cedce5be8c6e447c357bc07aa802b5a
Csaba Sulyok <csaba.sulyok@gmail.com>
2013-02-17 19:44:03 +0100

    Add preference page and popup item to set custom XML ruleset file.

---
+++ plugin/src/org/codenarc/eclipse/preferences/CodeNarcPreferenceConstants.groovy
@@ -0,0 +1,14 @@
+package org.codenarc.eclipse.preferences
+
+class CodeNarcPreferenceConstants {
+
+    public static final String P_CONFIG_PATH = 'org.codenarc.eclipse.configFilePath'
+    public static final String CONFIG_PATH_LABEL = '&CodeNarc XML ruleset config file:'
+    public static final String CONFIG_PATH_DEFAULT = 'D:/codenarc.xml'
+
+    public static final String P_USE_CUSTOM_CONFIG = 'org.codenarc.eclipse.useCustomConfig'
+    public static final String USE_CUSTOM_CONFIG_LABEL = '&Use custom CodeNarc XML ruleset config file:'
+    public static final boolean USE_CUSTOM_CONFIG_DEFAULT = false
+
+    public static final String PREFERENCE_PAGE_DESCRIPTION = 'CodeNarc preference page'
+}
