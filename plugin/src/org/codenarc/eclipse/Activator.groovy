13a26b525e77928e34c35ea38797de6e049c57f5
René Scheibe <rene.scheibe@gmail.com>
2014-09-16 19:30:09 +0200

    complete comments
    * using the ones from the generated Activator when creating a new plugin

--- plugin/src/org/codenarc/eclipse/Activator.groovy
+++ plugin/src/org/codenarc/eclipse/Activator.groovy
@@ -9,37 +9,45 @@ import org.osgi.framework.BundleContext
  */
 class Activator extends AbstractUIPlugin {
 
+    // The plug-in ID
     static final String PLUGIN_ID = 'CodeNarc'
 
     // The shared instance
     private static Activator plugin
 
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
     void start(BundleContext context) throws Exception {
         super.start(context)
         plugin = this
     }
 
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
     void stop(BundleContext context) throws Exception {
         plugin = null
         super.stop(context)
     }
 
     /**
-    * Returns the shared instance
-    *
-    * @return the shared instance
-    */
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
     static Activator getDefault() {
         plugin
     }
 
     /**
-    * Returns an image descriptor for the image file at the given
-    * plug-in relative path
-    *
-    * @param path the path
-    * @return the image descriptor
-    */
+     * Returns an image descriptor for the image file at the given plug-in relative path
+     *
+     * @param path the path
+     * @return the image descriptor
+     */
     static ImageDescriptor getImageDescriptor(String path) {
         imageDescriptorFromPlugin(PLUGIN_ID, path)
     }
