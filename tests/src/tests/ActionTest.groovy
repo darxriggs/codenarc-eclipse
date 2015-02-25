cbec0cb6ae50b6b6e0e7e589d994c50fc192aa7a
René Scheibe <rene.scheibe@gmail.com>
2015-02-25 23:56:03 +0100

    upgrade to CodeNarc v0.23.0

--- tests/src/tests/ActionTest.groovy
+++ tests/src/tests/ActionTest.groovy
@@ -64,7 +64,7 @@ class ActionTest extends EclipseTestCase {
         testProject.createGroovyTypeAndPackage(
             'test',
             "${className}.groovy",
-            "class ${className} { def TEST }\n"
+            "class ${className} { int TEST }\n"
         )
     }
 
